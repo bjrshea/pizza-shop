5dd4d51e6271e07bb488ba566af8d6e387b9f681
Brendan Shea <padma@email.com>
2018-08-24 13:49:06 -0700

    Finish logic by using price of toppings variable in prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 // Backend Logic:
-function Order(name, size, toppings) {
+function Order(name, price) {
   this.pizzaName = name;
-  this.pizzaSize = size;
-  this.pizzaToppings = toppings;
+  this.pizzaPrice = price;
+  // this.pizzaToppings = toppings;
+}
+
+Order.prototype.orderDetails = function() {
+  return this.pizzaName + ", your order is: $" + this.pizzaPrice + ". It should be delivered in about 45 minutes!";
 }
 
 // Frontend Logic:
@@ -10,6 +14,7 @@ $(document).ready(function(){
   $("#order-form").submit(function(event) {
     event.preventDefault();
 
+  // User inputs:
     var toppingsArray = [];
     var inputtedName = $("#user-name").val();
     var inputtedSize = $("#user-size").val();
@@ -18,6 +23,7 @@ $(document).ready(function(){
       toppingsArray.push(inputtedToppings);
     });
 
+  // Price of toppings & pizza size:
     var priceOfToppings = 0;
     if(toppingsArray.length === 1) {
       priceOfToppings += 1;
@@ -25,18 +31,36 @@ $(document).ready(function(){
       priceOfToppings += 2;
     } else if (toppingsArray.length === 3) {
       priceOfToppings += 3;
+    } else if (toppingsArray.length === 4) {
+      priceOfToppings += 4;
+    } else if (toppingsArray.length === 5) {
+      priceOfToppings += 5;
+    } else if (toppingsArray.length === 6) {
+      priceOfToppings += 6;
+    } else if (toppingsArray.length === 7) {
+      priceOfToppings += 7;
     } else {
       priceOfToppings += 0;
     }
-
-    var priceOfSize = 0;
-    if
-
     console.log(priceOfToppings);
 
-    var pizzaOrder = new Order(inputtedName, inputtedSize, toppingsArray);
+    var priceOfSize = 0;
+    if(inputtedSize === "small") {
+      priceOfSize = 8;
+    } else if (inputtedSize === "medium") {
+      priceOfSize = 10;
+    } else {
+      priceOfSize = 12;
+    }
+    console.log(priceOfSize);
+
+    var pizzaTotal = priceOfToppings + priceOfSize;
+    console.log(pizzaTotal);
 
 
+  // Pizza order:
+    var pizzaOrder = new Order(inputtedName, pizzaTotal);
 
+    $("#order-price").append(pizzaOrder.orderDetails());
   });
 });
