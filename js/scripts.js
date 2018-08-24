dcd88c01b3ba034d35206b2609a96048184880b8
Brendan Shea <padma@email.com>
2018-08-24 12:40:29 -0700

    Begin prototype for Order constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
 // Backend Logic:
+var toppingsArray = [];
+
 function Order(name, size, toppings) {
   this.pizzaName = name;
   this.pizzaSize = size;
-  // this.pizzaToppings = [];
+  this.pizzaToppings = toppings;
 }
 
+Order.prototype.orderPrice = function () {
+  for 
+}
+
+
+
 // Frontend Logic:
-$(document).ready(function(){
-  $("#order-form").submit(function(event){
+$(document).ready(function() {
+  $("#order-form").submit(function(event) {
     event.preventDefault();
 
-    var toppingsArray = [];
     var inputtedName = $("#user-name").val();
-    console.log(inputtedName);
     var inputtedSize = $("#user-size").val();
     $("input:checkbox[name=toppings]:checked").each(function(){
       var inputtedToppings = $(this).val();
