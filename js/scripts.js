6fcf83820c023e0ecd0fdd1094868321abd9dd90
Brendan Shea <padma@email.com>
2018-08-24 13:02:32 -0700

    Add if/else statements for the price of toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,42 @@
 // Backend Logic:
-var toppingsArray = [];
-
 function Order(name, size, toppings) {
   this.pizzaName = name;
   this.pizzaSize = size;
   this.pizzaToppings = toppings;
 }
 
-Order.prototype.orderPrice = function () {
-  for 
-}
-
-
-
 // Frontend Logic:
-$(document).ready(function() {
+$(document).ready(function(){
   $("#order-form").submit(function(event) {
     event.preventDefault();
 
+    var toppingsArray = [];
     var inputtedName = $("#user-name").val();
     var inputtedSize = $("#user-size").val();
-    $("input:checkbox[name=toppings]:checked").each(function(){
+    $("input:checkbox[name=toppings]:checked").each(function() {
       var inputtedToppings = $(this).val();
       toppingsArray.push(inputtedToppings);
     });
 
+    var priceOfToppings = 0;
+    if(toppingsArray.length === 1) {
+      priceOfToppings += 1;
+    } else if(toppingsArray.length === 2){
+      priceOfToppings += 2;
+    } else if (toppingsArray.length === 3) {
+      priceOfToppings += 3;
+    } else {
+      priceOfToppings += 0;
+    }
+
+    var priceOfSize = 0;
+    if
+
+    console.log(priceOfToppings);
+
     var pizzaOrder = new Order(inputtedName, inputtedSize, toppingsArray);
-    console.log(pizzaOrder);
+
+
 
   });
 });
