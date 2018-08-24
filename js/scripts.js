f688b981e4a89b3dd734a5f751ce97392b59903b
Brendan Shea <padma@email.com>
2018-08-24 08:59:21 -0700

    Start backend logic by creating an Order contructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 // Backend Logic:
+function Order(size, toppings) {
+  this.size = size
+  this.toppings = toppings
+}
 
 // Frontend Logic:
 $(document).ready(function(){
   $("#pizza-order").submit(function(event){
     event.preventDefault();
+    var userInput = new Order("#user-size", "user-toppings")
+    console.log(userInput)
   });
 });
